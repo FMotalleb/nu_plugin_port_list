db36e9637d8d5517db19871c2174b8c7bb604d3f
FMotalleb <m.falahnejad@dornica.net>
2024-05-05 11:14:57 +0330

    fix: bump nu-protocol to v0.93

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 mod port_list;
 
-use nu_plugin::{  PluginCommand};
 use crate::port_list::PortList;
+use nu_plugin::PluginCommand;
 
 pub struct PortListPlugin;
 
 impl nu_plugin::Plugin for PortListPlugin {
-    fn commands(&self) -> Vec<Box<dyn PluginCommand<Plugin=Self>>> {
-        vec![
-            Box::new(PortList::new())
-        ]
+    fn commands(&self) -> Vec<Box<dyn PluginCommand<Plugin = Self>>> {
+        vec![Box::new(PortList::new())]
     }
 }
 fn main() {
     nu_plugin::serve_plugin(&mut PortListPlugin {}, nu_plugin::MsgPackSerializer {})
 }
-
