f4303766ac2aec82718e9e239c668ba7c46251c7
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2024-06-27 00:25:01 +0330

    minor: bump nu deps to v0.95

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ impl nu_plugin::Plugin for PortListPlugin {
     fn commands(&self) -> Vec<Box<dyn PluginCommand<Plugin = Self>>> {
         vec![Box::new(PortList::new())]
     }
+
+    fn version(&self) -> String {
+        env!("CARGO_PKG_VERSION").into()
+    }
 }
 fn main() {
     nu_plugin::serve_plugin(&mut PortListPlugin {}, nu_plugin::MsgPackSerializer {})
